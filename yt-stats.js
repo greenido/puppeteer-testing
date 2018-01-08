395bd021a870fddd6686c16b5674d30f2011efc2
Ido Green <greenido@gmail.com>
2018-01-08 14:07:14 -0800

    With an option to save the total to a text file

--- yt-stats.js
+++ yt-stats.js
@@ -7,6 +7,11 @@
 // @see https://github.com/topics/puppeteer / https://github.com/emadehsan/thal
 
 const puppeteer = require("puppeteer");
+var SequelizePkg = require('sequelize'); 
+var fs = require('fs');
+
+var sequelize;
+var DB;
 
 //
 // Util to format numbers with commas
@@ -17,20 +22,42 @@ const numberWithCommas = (x) => {
 
   async function run() {    
     const browser = await puppeteer.launch({
-      headless: false
+      headless: true,
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
 
     const videoKeys = ["3NIopUsI4ik", "ykSFAs6S_Kg", "GKP4AS4L_K8", "cHEahGHseGc", 
-    "sjpbu4y6F_M", "PzILuAKyXIg", "ua4QGWmDfB8"];
+                       "sjpbu4y6F_M", "PzILuAKyXIg", "ua4QGWmDfB8", "end"];
     let totalViews = 0;
     for (key in videoKeys) {
-      let tmpStrViews = await getViewsPerPage(videoKeys[key]);
-      tmpStrViews = tmpStrViews.replace("views","");
-      tmpStrViews = tmpStrViews.replace(",","");
-      tmpStrViews = tmpStrViews.replace(" ","");
-      totalViews += Number.parseInt(tmpStrViews);
+      if (videoKeys[key] == "end") {
+        await saveTotal(totalViews);	    
+        break;
+      }
+      try {
+        let tmpStrViews = await getViewsPerPage(videoKeys[key]);
+        tmpStrViews = tmpStrViews.replace("views","");
+        tmpStrViews = tmpStrViews.replace(",","");
+        tmpStrViews = tmpStrViews.replace(" ","");
+        totalViews += Number.parseInt(tmpStrViews);
+      } catch(err) {
+        console.log("ERR with page: " + videoKeys[key] + " Error: " + JSON.stringify(err));
+      }
     }
-    console.log("** The Total is: " + numberWithCommas(totalViews) + " views");
+   
+	  
+    async function saveTotal(totalViews) {
+      var ts1 = Math.round((new Date()).getTime() );
+      var totalViews = "" + totalViews;
+      fs.writeFile('./totals.txt', totalViews, (err) => {
+        if (err) {
+	  console.log("ERROR with saving the totals: " + totalViews + " Err: " + JSON.stringify(err));	
+	}
+        console.log("The totals: " + totalViews + " was succesfully saved!");
+      }); 
+      DB.create({'id': ts1, 'total': totalViews, 'comment': 'totals'});
+      console.log("time: " + ts1 + " " + totalViews + " ** The Total is: " + numberWithCommas(totalViews) + " views");
+    }
 
     //
     //
@@ -55,11 +82,58 @@ const numberWithCommas = (x) => {
       }, VIEWS_SELECTOR);
 
       console.log("The views for " +key + " video are: " + views);
+      var ts = Math.round((new Date()).getTime() ); 
+      DB.create({ id: ts, total: views, comment: key});
       return views;
     }
     
     browser.close();
-    process.exit(0);
+    //process.exit(0);
 }
 
-run();
+// CREATE TABLE IF NOT EXISTS `stats` (`id` INTEGER PRIMARY KEY, `total` INTEGER, `comment` VARCHAR(255), `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NUL
+// L);
+//
+async function connectDB(){
+  sequelize = new SequelizePkg('database','' , '', {
+     host: '0.0.0.0',
+     dialect: 'sqlite',
+     pool: {
+       max: 15,
+       min: 0,
+       idle: 90000
+      },
+       storage: './database.db'
+   });
+
+   sequelize.authenticate()
+    .then(function(err) {
+        console.log('Connection has been established successfully to our DB');
+        DB = sequelize.define('stats', {
+        id: {
+    	  type: SequelizePkg.INTEGER, primaryKey: true
+        },
+        total: {
+          type: SequelizePkg.STRING
+        },
+        comment: { 
+          type: SequelizePkg.STRING
+        }
+      });
+
+        DB.sync({force: false}).then(() => {
+          console.log('** Synced with the database.');  
+      });
+    }); 
+}
+
+//
+// start the party
+// 
+try {
+  connectDB().then(run());
+}
+catch(err) {
+  console.log("MAIN ERR: " + JSON.stringify(err));
+}
+
